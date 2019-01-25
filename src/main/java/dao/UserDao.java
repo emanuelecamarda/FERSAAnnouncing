231dcb97a3dbe90da2e79a4396d1304b5354bb0e
stefano <stefano-2310@libero.it>
2019-01-25 15:09:02 +0100

    aggiunto metodo SignUp funziona ma c'è un problema in rs riga 93 commento

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -70,4 +70,67 @@ public class UserDao {
 
         return u;
     }
+
+    public User SignUp(String nickname, String nome , String cognome , String email , String password , Character gender) {
+        Statement stmt = null;
+        Connection conn = null;
+        User v = null;
+        try {
+            conn = this.ds.getConnection();
+
+
+
+            stmt = conn.prepareStatement("insert into \"public\".\"Users\" (nickname , nome , cognome , email , password , gender) " +
+                    "values (?,?,?,?,?,?);", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ((PreparedStatement) stmt).setString(1, nickname);
+            ((PreparedStatement) stmt).setString(2, nome);
+            ((PreparedStatement) stmt).setString(3, cognome);
+            ((PreparedStatement) stmt).setString(4, email);
+            ((PreparedStatement) stmt).setString(5, password);
+            ((PreparedStatement) stmt).setString(6, String.valueOf(gender));
+            ResultSet rs = ((PreparedStatement) stmt).executeQuery();
+
+            //problema : questa query funziona ovvero inserisce nella tabella ho controllato solo che non ritorna nessun risultato
+            //ovvero rs è sempre vuoto quindi ho sempre un return null
+
+            if (!rs.first()) // rs empty
+                return null;
+
+            //boolean moreThanOne = rs.first() && rs.next();
+
+
+            //rs.first();
+
+//            String nome = rs.getString("nome");
+//            String cognome = rs.getString("cognome");
+//            String nicknameLoaded = rs.getString("nickname");
+//            String email = rs.getString("email");
+//            Character gender = rs.getString("gender").toCharArray()[0];
+
+
+            v = new User(nickname, nome, cognome, email, "", gender);
+
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+
+        return v;
+    }
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -7,6 +7,8 @@ public class Test {
     public static void main(String args[]) {
         UserDao userDao = new UserDao();
         User u = userDao.findByNicknameAndPassword("bella", "bella");
+        User v = userDao.SignUp("ciao", "hello ","bye" , "aaabbb" , "abcd" , 'm');
         System.out.println(u);
+        System.out.println(v);
     }
 }
