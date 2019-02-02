7e7da63d6ebd1616c8a728ef7660502e18f1c1c7
emanuele <emanuelecamarda@gmail.com>
2019-02-02 12:47:23 +0100

    merge Users

--- src/main/java/boundary/LoginBoundary.java
+++ src/main/java/boundary/LoginBoundary.java
@@ -48,6 +48,10 @@ public class LoginBoundary {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    public void changeSignup() {
 
     }
 }
--- src/main/java/dao/ApartmentDao.java
+++ src/main/java/dao/ApartmentDao.java
@@ -147,5 +147,47 @@ public class ApartmentDao {
         return Boolean.FALSE;
     }
 
+    /**
+     * Edit by EC. Return the max ID for this entity. Use for initialize the ID generator.
+     * @return
+     */
+    public Integer getMaxID() {
+        PreparedStatement stmt = null;
+        Connection conn = null;
+        Integer maxID = null;
+
+        try {
+            conn = this.ds.getConnection();
+
+            stmt = conn.prepareStatement("select MAX(\"ID\") from \"public\".\"Apartment\";",
+                    ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet result = stmt.executeQuery();
+
+            if (result.next() && maxID == null) {
+                maxID = result.getInt(1);
+            }
+            result.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+
+        return maxID;
+    }
 
 }
