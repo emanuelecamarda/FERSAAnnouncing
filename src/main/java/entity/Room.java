8280f92ca43a46b5ef2d1400112a2441bc4536e5
stefano <stefano-2310@libero.it>
2019-02-02 12:09:29 +0100

    errori sul create in roomdao e create in apartmentdao

--- src/main/java/entity/Room.java
+++ src/main/java/entity/Room.java
@@ -6,4 +6,41 @@ public class Room extends Announce {
     private int roomersNumber;
     private boolean privateBathroom;
     private List[] roomers;
+
+    public Room(int ID, String city, String address, Double price, String description, double size, boolean available, int roomersNumber, boolean privateBathroom, List[] roomers) {
+        super(ID, city, address, price, description, size, available);
+        this.roomersNumber = roomersNumber;
+        this.privateBathroom = privateBathroom;
+        this.roomers = roomers;
+    }
+
+    public Room(int roomersNumber, boolean privateBathroom, List[] roomers) {
+        this.roomersNumber = roomersNumber;
+        this.privateBathroom = privateBathroom;
+        this.roomers = roomers;
+    }
+
+    public int getRoomersNumber() {
+        return roomersNumber;
+    }
+
+    public void setRoomersNumber(int roomersNumber) {
+        this.roomersNumber = roomersNumber;
+    }
+
+    public boolean isPrivateBathroom() {
+        return privateBathroom;
+    }
+
+    public void setPrivateBathroom(boolean privateBathroom) {
+        this.privateBathroom = privateBathroom;
+    }
+
+    public List[] getRoomers() {
+        return roomers;
+    }
+
+    public void setRoomers(List[] roomers) {
+        this.roomers = roomers;
+    }
 }
